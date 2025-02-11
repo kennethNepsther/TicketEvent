8fbd7c23f4bafe3bc969996bc8fd8c52ef459f31
kenneth luzolo <kenneth.nepsther@gmail.com>
2025-02-11 23:42:30 +0100

    :tada: Initial Commit

---
+++ src/test/java/com/ticketevent/TicketEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
